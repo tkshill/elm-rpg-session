2b5904ad5d9169ea9c2ab68b0f4fc6c0d49cd6c1
Kirk Shillingford <tkshillinz@icloud.com>
2023-10-04 13:03:18 -0600

    getting there!

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,7 +4,13 @@ import Expect exposing (..)
 import Mark exposing (Outcome(..))
 import Mark.Error
 import Test exposing (..)
-import Unnatural exposing (MarkupArchetype, PortfolioMarkupElement(..))
+import Element.Region exposing (description)
+
+
+type PortfolioTestMarkupElement
+    = PortfolioNameMarkup String
+    | PortfolioArchetypeMarkup (List { id : Int, name : String })
+    | PortfolioArchetypeMarkup2 (List { id : Int, name : String, description : String })
 
 
 example : Test
@@ -49,8 +55,8 @@ simpleMarkupParse =
         Expect.equal { text = "Sample\nParagraph\nParagraph" } expected
 
 
-samplePortfolioMarkup2 : String
-samplePortfolioMarkup2 =
+samplePortfolioMarkup4 : String
+samplePortfolioMarkup4 =
     """
 |> Name
     The Unnatural
@@ -86,7 +92,7 @@ samplePortfolioMarkup =
 """
 
 
-samplePortfolio : Result (List String) { name : String, archetypes : List MarkupArchetype }
+samplePortfolio : Result (List String) { name : String, archetypes : List { id : Int, name : String } }
 samplePortfolio =
     { name = "The Unnatural"
     , archetypes =
@@ -98,13 +104,16 @@ samplePortfolio =
         |> Result.Ok
 
 
-parsePortfolioMarkupElements : List PortfolioMarkupElement -> Maybe { name : String, archetypes : List MarkupArchetype }
+parsePortfolioMarkupElements : List PortfolioTestMarkupElement -> Maybe { name : String, archetypes : List { id : Int, name : String } }
 parsePortfolioMarkupElements elements =
     case elements of
-        [ PortfolioNameMarkup name, PortfolioArchetypesMarkup archetypes ] ->
+        [ PortfolioNameMarkup name, PortfolioArchetypeMarkup archetypes ] ->
             Just { name = name, archetypes = archetypes }
 
-        [ PortfolioArchetypesMarkup archetypes, PortfolioNameMarkup name ] ->
+        [ PortfolioArchetypeMarkup archetypes, PortfolioNameMarkup name ] ->
+            Just { name = name, archetypes = archetypes }
+
+        [ PortfolioArchetypeMarkup2 archetypes, PortfolioNameMarkup name ] ->
             Just { name = name, archetypes = archetypes }
 
         _ ->
@@ -118,21 +127,12 @@ portfolioMarkupParse =
             name =
                 Mark.block "Name" PortfolioNameMarkup Mark.string
 
-            -- exampleList =
-            --     Mark.tree "List" (\(Mark.Enumerated list) -> List.map (\(Mark.Item item) -> Maybe.withDefault "" (List.head item.content)) list.items) Mark.string
-            -- archetype =
-            --     Mark.record "Archetype"
-            --         (\n d e -> { name = n, description = d, examples = e })
-            --         |> Mark.field "name" Mark.string
-            --         |> Mark.field "description" Mark.string
-            --         |> Mark.field "examples" exampleList
-            --         |> Mark.toBlock
             archetype : Mark.Block String
             archetype =
                 Mark.block "Archetype" identity Mark.string
 
             archetypes =
-                Mark.tree "Archetypes" (\(Mark.Enumerated list) -> List.concatMap (\(Mark.Item item) -> List.indexedMap (\i c -> { id = i, name = c }) item.content) list.items) archetype |> Mark.map PortfolioArchetypesMarkup
+                Mark.tree "Archetypes" (\(Mark.Enumerated list) -> List.concatMap (\(Mark.Item item) -> List.indexedMap (\i c -> { id = i, name = c }) item.content) list.items) archetype |> Mark.map PortfolioArchetypeMarkup
 
             -- |> Mark.map PortfolioArchetypesMarkup
             document =
@@ -140,6 +140,69 @@ portfolioMarkupParse =
 
             expected =
                 case Mark.compile document samplePortfolioMarkup of
+                    Success doc ->
+                        Result.fromMaybe [] doc
+
+                    Almost partial ->
+                        List.map Mark.Error.toString partial.errors |> Result.Err
+
+                    Failure failure ->
+                        List.map Mark.Error.toString failure |> Result.Err
+        in
+        Expect.equal samplePortfolio expected
+
+
+samplePortfolioMarkup2 : String
+samplePortfolioMarkup2 =
+    """
+|> Name
+    The Unnatural
+
+|> Archetypes
+    1.  |> Archetype
+            
+            name = Unnatural Sustenance
+
+            description = Choose a substance that you need to consume to stay alive, or sane. Talk with your steward to determine the parameters of your need.
+"""
+
+samplePortfolio2 : Result (List String) { name : String, archetypes : List { id : Int, name : String, description: String } }
+samplePortfolio2 =
+    { name = "The Unnatural"
+    , archetypes =
+        [ { id = 0
+          , name = "Unnatural Sustenance"
+          , description = "Choose a substance that you need to consume to stay alive, or sane. Talk with your steward to determine the parameters of your need."`
+          }
+        ]
+    }
+        |> Result.Ok
+
+portfolioMarkupParse2 : () -> Expectation
+portfolioMarkupParse2 =
+    \_ ->
+        let
+            name =
+                Mark.block "Name" PortfolioNameMarkup Mark.string
+
+            archetype =
+                Mark.record "Archetype"
+                    (\n -> { name = n })
+                    |> Mark.field "name" Mark.string
+                    |> Mark.field "description" Mark.string
+                    |> Mark.toBlock
+
+            archetypes =
+                Mark.tree "Archetypes"
+                    (\(Mark.Enumerated list) -> List.concatMap (\(Mark.Item item) -> List.indexedMap (\i c -> { id = i, name = c.name, description = c.description }) item.content) list.items)
+                    archetype
+                    |> Mark.map PortfolioArchetypeMarkup2
+
+            document =
+                Mark.document identity (Mark.manyOf [ name, archetypes ] |> Mark.map parsePortfolioMarkupElements)
+
+            expected =
+                case Mark.compile document samplePortfolioMarkup2 of
                     Success doc ->
                         Result.fromMaybe [] doc
 
