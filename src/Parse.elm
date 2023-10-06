9d3f59cc20be060742c105ccc4760232de079ba8
Kirk Shillingford <tkshillinz@icloud.com>
2023-10-05 20:05:59 -0600

    updated parser

--- src/Parse.elm
+++ src/Parse.elm
@@ -1,5 +1,7 @@
 module Parse exposing (..)
 
+import Mark
+
 
 type PortfolioName
     = TheUnnatural -- Audacity/Oddity/Empathy/Acuity
@@ -16,8 +18,57 @@ type alias Archetype =
     { id : Int, name : String, description : String, examples : List String, karma : String }
 
 
+type alias Stats =
+    { oddity : Int, acuity : Int, audacity : Int, empathy : Int }
+
+
 type PortfolioMarkupElement
-    = Base { name : PortfolioName, description : String, flavour : String }
+    = Core { name : PortfolioName, description : String, flavour : String }
     | Archetypes { name : String, description : String, options : List Archetype }
-    | Stats { oddity : Int, acuity : Int, audacity : Int, empathy : Int }
+    | StatGroup { oddity : Int, acuity : Int, audacity : Int, empathy : Int }
     | Moves (List Move)
+
+
+type HarmDealt
+    = Light
+    | Hard
+    | Heavy
+    | Humongous
+
+
+type Range
+    = Touch
+    | Close
+    | Far
+
+
+type Speed
+    = Fast
+    | Slow
+
+
+type alias Attack =
+    { id : Int, name : String, givenName : String, harm : Harm, range : Range, speed : Speed }
+
+
+type alias Harm =
+    { value : Int, description : String }
+
+
+type alias Portfolio =
+    { name : PortfolioName
+    , description : String
+    , flavour : String
+    , archetypes : List Archetype
+    , stats : Stats
+    , moves : List Move
+    , attacks : List Attack
+    , karma : Int
+    , hurt : Int
+    , harm : Harm
+    }
+
+
+
+-- portfolioDocument : Mark.Document Portfolio
+-- portfolioDocument = Mark.document
